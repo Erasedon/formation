91da25174cd9a18be7cb95bf9a708193ae5a80b0
Hassank22 <hassank.kabul@gmail.com>
2023-01-18 09:55:28 +0100

    Page detail

---
+++ page-detail.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page-Detail</title>
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+    <div class="container">
+        <div class="row ">
+
+            <nav class="navbar navbar-expand-lg  navbar-page-detail">
+                <div class="container-fluid">
+                    <a href="#" class="navbar-brand">Formation</a>
+                    <button type="button" class="navbar-toggler" data-bs-toggle="collapse"
+                        data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <div class="navbar-nav">
+                            <a href="#" class="nav-item nav-link active">Accueil</a>
+                            <a href="#" class="nav-item nav-link">Nos formations</a>
+
+                        </div>
+                        <div class="navbar-nav ms-auto">
+                            <a href="#" class="nav-item nav-link">Nous contacter</a>
+                            <a href="#" class="nav-item nav-link">A propos de nous</a>
+
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+
+            <!-- <section>
+                <div class="p-5 mb-4  rounded-3">
+                    <div class="">
+                        <h1 class="display-5 fw-bold page-detail-titre">DÉVELOPPEUR WEB ET WEB MOBILE</h1>
+                        <p class="col-md-8 fs-4"> <strong> Niveau 5</strong> <br> Formation initiale dans le cadre du
+                            Plan Régional de Formation Statut de stagiaire de la formation professionnelle</p>
+                    </div>
+                </div>
+            </section> -->
+
+            <section class="test">
+
+                <div class="container my-5">
+                    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+                        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+                            <h1 class="display-4 fw-bold lh-1">BDÉVELOPPEUR WEB ET WEB MOBILE</h1>
+                            <p class="lead"><strong> Niveau 5</strong> <br> Formation initiale dans le cadre du
+                                Plan Régional de Formation Statut de stagiaire de la formation professionnelle</p>
+
+                        </div>
+
+
+
+<div class="container"> 
+<div class="row">
+    
+ <div class="col-md-2">
+                      
+
+<div class="p-2 mb-3 titre-RNCP  ">
+    <p> RNCP31114BC01
+        Analyser et définir la
+        stratégie d'un système
+        d'information
+    </p>
+
+</div>
+
+<div class="p-2 mb-3 titre-RNCP">
+    <p> RNCP31114BC02
+        Assurer la gestion du
+        Système d’information
+    </p>
+
+</div>
+</div>
+
+<div class="col-md-8">
+<div class="p-4 mb-3  rounded-3">
+
+    <h4 class="titre-comptetence">Compétences</h4>
+    <p class="">- Maquetter une application.<br>
+        - Réaliser une interface utilisateur web statique et adaptable.<br>
+        - Développer une interface utilisateur web dynamique.<br>
+        - Réaliser une interface utilisateur avec une solution de gestion de contenu ou e-commerce <br>
+        <br>
+
+        - Créer une base de données.<br>
+        - Développer les composants d'accès aux données.<br>
+        - Développer la partie back-end d'une application web ou web mobile.<br>
+        - Elaborer et mettre en œuvre des composants dans une application de gestion de contenu ou
+        e-commerce.
+
+    </p>
+</div>
+</div>
+
+<div class="col-md-2 ">
+
+<div class=''>
+    <div class="card card-page-detail" style="width: 12rem;">
+        <div class="card-body card-page-detail">
+
+            <p class="card-title">CODE RNCP 31114</p>
+
+        </div>
+    </div>
+</div>
+<hr>
+
+
+
+<div class=''>
+    <div class="card card-page-detail" style="width: 12rem;">
+        <div class="card-body card-page-detail">
+
+            <p class="card-title"> <strong> </strong>Durée de la formation:</p>
+            <p class="card-text">987 h 00 dont 140 h 00 en entrepriset.</p>
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+</div>
+
+</div>
+
+
+<div class='container'>
+
+<div class='row'>
+
+<div class='col-md-6'>
+
+    <div class="p-4 mb-3  rounded-3">
+
+        <h4 class="titre-comptetence">Conditions d’admission et pré-requis</h4>
+        <p class=""> - Très forte motivation, à prouver !<br>
+            - Aucune condition de diplôme <br>
+            - Tests en ligne <br>
+            - Entretien de sélection <br>
+            - Forte appétence pour le numérique</p>
+    </div>
+
+</div>
+
+
+<div class='col-md-6'>
+    <div class="p-4 mb-3  rounded-3">
+        <h4 class="titre-comptetence">Métiers visés</h4>
+        <p class="">- Développeur web et web mobile<br>
+            - Développeur logiciel <br>
+            - Développeur front<br>
+            - Développeur backend</p>
+    </div>
+</div>
+
+<div class='col-md-6'>
+
+
+    <div class="p-4 mb-3  rounded-3">
+        <h4 class="titre-comptetence">Frais de scolarité</h4>
+        <p class="">Pris en charge par la région Grand Est
+            Rémunération pour l’apprenant (ASP)</p>
+    </div>
+</div>
+
+<div class='col-md-6'>
+    <div class="p-4 mb-3  rounded-3">
+        <h4 class="titre-comptetence">Lieu</h4>
+        <p class="">Pôle Formation UIMM – Campus Sup Ardenne 8 rue Claude Chrétien- 08000
+            Charleville-Mézières</p>
+    </div>
+</div>
+
+ </div>
+
+</div> 
+
+
+</div>
+</div>
+
+
+
+
+
+
+                
+
+
+          
+ 
+
+
+
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-muted">&copy; 2023 Formation</p>
+
+            <a href="/"
+                class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                <svg class="bi me-2" width="40" height="32">
+                    <use xlink:href="#bootstrap" />
+                </svg>
+            </a>
+
+            <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Accueil</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Nos formations</a></li>
+
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">A propos de nous</a></li>
+            </ul>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+</body>
+
+</html>
