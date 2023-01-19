792d7d5f260cbd27ebe4f4a1925375d886f66545
yanis.pirlet <yanis.pirlet@gmail.com>
2023-01-19 14:05:08 +0100

    requete et filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,7 @@
             var rech =str ;
             if (action != "") { datas.action = action;}
             if (str = !null) { datas.str = rech;}
-            
-            
+        
             $.ajax({
                 url: "assets/include/traitement_search.php",
                 method: "GET",
@@ -27,10 +26,11 @@
         }
         $('.livesearch').html('<div id="loading" style="" ></div>');
         var action = 'fetch_data';
+        var filter = "sansf";
         var rech =str ;
         if (action = !null) { datas.action = action;}
         if (str = !null) { datas.str = rech;}
-        
+        if (filter != "") { datas.filter = filter;}
         
         $.ajax({
             url: "assets/include/traitement_search.php",
@@ -39,7 +39,28 @@
             success: function(data) {
                 // document.getElementById("livesearch").innerHTML=this.responseText;
                 $('.livesearch').html(data);
-                console.log('valeur ='+data);
             }
         });
     }
+// recuperer la valeur des checkbox 
+function get_filter(class_name) {
+    // un array vide mit dans la variable filter & dans le tableau du class_name tout les checkbox true sont ajouter a la variable checkbox
+    var filter = [];
+    var checkbox = $('.' + class_name + ':checked'); 
+    // ensuite on verifie si chexbox  est vide donc l'url sera vider et on modifie l'historique pour que l'utilisateur si retrouve 
+    if(checkbox.length < 1) {
+        history.pushState({}, '', url);
+        url.searchParams.delete(class_name);
+
+    } else {
+        // sinon si checkbox n'est pas vide  on  modifie l'url en ajoutant class_name = filter sans oublier historique .
+        checkbox.each(function(i) {
+            filter.push($(this).val());
+        });
+
+        url.searchParams.set(class_name, filter.toString());         
+        history.pushState({}, '', url);
+    }
+    // on retourne le tableau sous la forme  d'une chaine de caractere 
+    return filter.toString();
+}
