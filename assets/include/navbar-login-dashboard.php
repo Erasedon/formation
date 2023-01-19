fee0ceb3044d5f1a471995d5178aa5ef90249977
Hassank22 <hassank.kabul@gmail.com>
2023-01-19 14:35:36 +0100

    login supprimer navbar

--- assets/include/navbar-login-dashboard.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar bg-body-tertiary titre-page-login-dashboard">
-  <div class="container titre-page-login-dashboard">
-    <a class="navbar-brand titre-page-login-dashboard" href="#">
-        <h5> Formation</h5>
-    </a>
-  </div>
-</nav>
