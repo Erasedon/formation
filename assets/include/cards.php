944ddd1e0ce636ed8f49cec917a4dce086d4894c
Hassank22 <hassank.kabul@gmail.com>
2023-01-20 15:38:26 +0100

    frontend page accueil

---
+++ assets/include/cards.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cards</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+    <div class="container">
+
+        <div class="row">
+
+            <!-- <div class="col-md-4">
+                <div class="cards-block">
+                 <p>Categorie</p>
+                    <p>REFERENT DIGITAL</p>
+                </div>
+
+            </div> -->
+
+            <div class="col-md-4 p-2 mb-3">
+                <div class="cards-block">
+                    <h2> Categorie</h2>
+                    <p>DÉVELOPPEUR WEB ET WEB MOBILE </p>
+                    <div class="sous-cards-block">
+
+<p>Niveau diplome</p>
+<p>Durée formation</p>
+</div>
+                </div>
+               
+
+            </div>
+
+
+
+
+
+            <div class="col-md-4 p-2 mb-3">
+                <div class="cards-block">
+                    <h2> Categorie</h2>
+                    <p>DÉVELOPPEUR WEB ET WEB MOBILE
+                    </p>
+                    <div class="sous-cards-block">
+
+<p>Niveau diplome</p>
+<p>Durée formation</p>
+</div>
+                </div>
+
+            </div>
+            <div class="col-md-4 p-2 mb-3">
+                <div class="cards-block">
+                    <h2> Categorie</h2>
+                    <p>TECHNICIEN SUPÉRIEUR SYSTÈMES
+                        ET RÉSEAUX
+                        Spécialisation Analyste Cybersécurité
+                    </p>
+                    <div class="sous-cards-block">
+
+<p>Niveau diplome</p>
+<p>Durée formation</p>
+</div>
+                </div>
+
+            </div>
+            <div class="col-md-4 p-2 mb-3">
+                <div class="cards-block">
+                    <h2> Categorie</h2>
+                    <p>CONCEPTEUR DESIGNER UI
+                    </p>
+                    <div class="sous-cards-block">
+
+<p>Niveau diplome</p>
+<p>Durée formation</p>
+</div>
+                </div>
+
+            </div>
+            <div class="col-md-4 p-2 mb-3">
+                <div class="cards-block">
+                    <h2> Categorie</h2>
+                    <p>CONCEPTEUR DEVELOPPEUR
+                        D’APPLICATION</p>
+                        <div class="sous-cards-block">
+
+<p>Niveau diplome</p>
+<p>Durée formation</p>
+</div>
+                </div>
+
+            </div>
+            <div class="col-md-4 p-2 mb-3">
+                <div class="cards-block">
+                    <h2> Categorie</h2>
+                    <p>ADMINISTRATEUR SYSTÈMES,
+                        RÉSEAUX ET BASES DE DONNÉES</p>
+
+                        <div class="sous-cards-block">
+
+<p>Niveau diplome</p>
+<p>Durée formation</p>
+</div>
+                      
+                </div>
+             
+            </div>
+
+
+        </div>
+
+
+
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
--- assets/include/recherche.php
+++ assets/include/recherche.php
@@ -1,2 +1,29 @@
-<input type="text" id="rech" onkeyup="showResult(this.value)">
-<div class="livesearch"></div>
+
+
+<!-- <input type="text" id="rech" onkeyup="showResult(this.value)">
+<div class="livesearch"></div> -->
+
+
+<div class="px-4 py-2 my-3 text-center">
+ 
+ <h1 class="display-5 fw-bold">Trouvez nos formations</h1>
+ <div class="col-lg-6 mx-auto">
+   <p class="lead mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. </p>
+   <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+
+   </div>
+ </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 p-2 my-3">
+
+      <input type="text" autocomplete="off" placeholder="Rechercher ..." style="width:50%" id="rech"  onkeyup="showResult(this.value)">
+       <!-- <button type="#" class="search" disabled></button> -->
+
+      <div class="livesearch"></div> 
+     
+      </div>
+    </div>
+  </div>
