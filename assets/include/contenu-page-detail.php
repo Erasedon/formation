bedfbaaca6c8622ebfea11a5f8664ed70eb8db31
yanis.pirlet <yanis.pirlet@gmail.com>
2023-01-23 03:00:07 +0100

    contenu page details fini

--- assets/include/contenu-page-detail.php
+++ assets/include/contenu-page-detail.php
@@ -1,7 +1,8 @@
 <?php 
+include('assets/db/connectdb.php');	
  $query = "
  SELECT * FROM formation f, niveau n,appartenir_cat acat, categories c, competence com,avoir_ref aref,reference ref,posseder p,effectuer_type_formation etf,type_formation tf 
- WHERE f.id_niveau = n.id_niveau AND acat.id_categories = c.id_categories AND acat.id_formation = f.id_formation AND aref.id_competence = com.id_competence AND aref.id_reference = ref.id_reference AND  p.id_competence = com.id_competence AND p.id_formation = f.id_formation AND etf.id_type_formation = tf.id_type_formation AND etf.id_formation = f.id_formation 
+ WHERE f.id_niveau = n.id_niveau AND acat.id_categories = c.id_categories AND acat.id_formation = f.id_formation AND aref.id_competence = com.id_competence AND aref.id_reference = ref.id_reference AND  p.id_competence = com.id_competence AND p.id_formation = f.id_formation AND etf.id_type_formation = tf.id_type_formation AND etf.id_formation = f.id_formation AND f.id_formation ='".$_GET["id_formation"]."'
  ";
  $statement =$db->prepare($query);
  $statement->execute();
@@ -11,8 +12,8 @@
 <div class="container my-5">
     <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
         <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-            <h1 class="display-4 fw-bold lh-1">BDÉVELOPPEUR WEB ET WEB MOBILE</h1>
-            <p class="lead"><strong> Niveau 5</strong> <br> Formation initiale dans le cadre du
+            <h1 class="display-4 fw-bold lh-1"><?php echo $result['titre_formation'];  ?></h1>
+            <p class="lead"><strong> <?php echo $result['titre_niveau'];  ?></strong> <br> Formation initiale dans le cadre du
                 Plan Régional de Formation Statut de stagiaire de la formation professionnelle</p>
 
         </div>
@@ -24,18 +25,19 @@
 
 
                     <div class="p-2 mb-3 titre-RNCP  ">
-                        <p> RNCP31114BC01
-                            Analyser et définir la
-                            stratégie d'un système
-                            d'information
+                        <p> <?php 
+                        
+                        echo $result['numeros_reference'];  
+                        echo $result['titre_reference'];  ?>
                         </p>
 
                     </div>
 
                     <div class="p-2 mb-3 titre-RNCP">
-                        <p> RNCP31114BC02
-                            Assurer la gestion du
-                            Système d’information
+                        <p><?php 
+                        
+                        echo $result['numeros_reference'];  
+                        echo $result['titre_reference'];  ?>
                         </p>
 
                     </div>
@@ -44,19 +46,8 @@
                 <div class="col-md-8">
                     <div class="p-4 mb-3  rounded-3">
 
-                        <h4 class="titre-comptetence">Compétences</h4>
-                        <p class="">- Maquetter une application.<br>
-                            - Réaliser une interface utilisateur web statique et adaptable.<br>
-                            - Développer une interface utilisateur web dynamique.<br>
-                            - Réaliser une interface utilisateur avec une solution de gestion de contenu ou e-commerce
-                            <br>
-                            <br>
-
-                            - Créer une base de données.<br>
-                            - Développer les composants d'accès aux données.<br>
-                            - Développer la partie back-end d'une application web ou web mobile.<br>
-                            - Elaborer et mettre en œuvre des composants dans une application de gestion de contenu ou
-                            e-commerce.
+                        <h4 class="titre-comptetence"><?php echo $result['titre_competence'];  ?></h4>
+                        <p class=""><?php echo $result['desc_competence'];  ?>
 
                         </p>
                     </div>
@@ -82,7 +73,7 @@
                             <div class="card-body card-page-detail">
 
                                 <p class="card-title"> <strong> </strong>Durée de la formation:</p>
-                                <p class="card-text">987 h 00 dont 140 h 00 en entrepriset.</p>
+                                <p class="card-text"><?php echo $result['duree_formation'];  ?></p>
                             </div>
                         </div>
                     </div>
@@ -103,11 +94,7 @@
                     <div class="p-4 mb-3  rounded-3">
 
                         <h4 class="titre-comptetence">Conditions d’admission et pré-requis</h4>
-                        <p class=""> - Très forte motivation, à prouver !<br>
-                            - Aucune condition de diplôme <br>
-                            - Tests en ligne <br>
-                            - Entretien de sélection <br>
-                            - Forte appétence pour le numérique</p>
+                        <p class=""> <?php echo $result['condition_formation'];  ?></p>
                     </div>
 
                 </div>
@@ -116,10 +103,7 @@
                 <div class='col-md-6'>
                     <div class="p-4 mb-3  rounded-3">
                         <h4 class="titre-comptetence">Métiers visés</h4>
-                        <p class="">- Développeur web et web mobile<br>
-                            - Développeur logiciel <br>
-                            - Développeur front<br>
-                            - Développeur backend</p>
+                        <p class=""><?php echo $result['metier_viser_formation'];  ?></p>
                     </div>
                 </div>
 
@@ -128,16 +112,14 @@
 
                     <div class="p-4 mb-3  rounded-3">
                         <h4 class="titre-comptetence">Frais de scolarité</h4>
-                        <p class="">Pris en charge par la région Grand Est
-                            Rémunération pour l’apprenant (ASP)</p>
+                        <p class=""><?php echo $result['frais_scolarite_formation'];  ?></p>
                     </div>
                 </div>
 
                 <div class='col-md-6'>
                     <div class="p-4 mb-3  rounded-3">
                         <h4 class="titre-comptetence">Lieu</h4>
-                        <p class="">Pôle Formation UIMM – Campus Sup Ardenne 8 rue Claude Chrétien- 08000
-                            Charleville-Mézières</p>
+                        <p class=""><?php echo $result['lieu_formation'];  ?></p>
                     </div>
                 </div>
 
@@ -148,3 +130,4 @@
 
     </div>
 </div>
+<?php  ?>
